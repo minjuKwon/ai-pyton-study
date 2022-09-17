69a9543b13dbee28412e849c46b517c21e580431
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-09-17 15:47:08 +0900

    Create data_Library_numpyArray2.py

---
+++ 2022_elice_AIEdu/dataLibrary/data_Library_numpyArray2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+print("1차원 array")
+array=np.array(range(10))
+
+print(array)
+print(type(array))
+print(array.ndim)
+print(array.shape)
+print(array.size)
+print(array.dtype)  
+print(array[5])
+print(array[3:6])
