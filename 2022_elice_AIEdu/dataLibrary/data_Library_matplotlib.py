194ade11576943f7e27e370fccdd435236d2b91c
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-09-17 17:52:17 +0900

    Create data_Library_matplotlib.py

---
+++ 2022_elice_AIEdu/dataLibrary/data_Library_matplotlib.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.arange(10)
+fig,ax=plt.subplots()
+
+ax.plot(
+        x,x,label='y=x',
+        linestyle='-',
+        marker='.',
+        color='blue'
+        )
+
+ax.plot(
+        x,x**2,label='y=x^2',
+        linestyle='-.',
+        marker=',',
+        color='red'
+        )
+
+ax.set_xlabel("x")
+ax.set_ylabel("y")
