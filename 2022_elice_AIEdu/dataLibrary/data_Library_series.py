c4a7d58d5677c205d02b785f1fbe7111b89e1417
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-09-17 16:38:03 +0900

    Create data_Library_series.py

---
+++ 2022_elice_AIEdu/dataLibrary/data_Library_series.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+country=pd.Series([5180,12718,141500,32676],
+                  index=['korea','japan','china','usa'],
+                  name="country")
+
+print(country,"\n")
